f72a145336f26852631317328a691ff070219229
내 이름 <you@example.com>
2020-07-08 12:28:55 +0900

    fabcar_web

--- routes/Router.js
+++ routes/Router.js
@@ -1,8 +1,26 @@
 var router = require('express').Router()
+var queryUtil = require('../utils/Util')
 
-router.get('/', (req, res, next)=>{
-    res.render('index')
+router.get('/', async (req, res , next)=>{
+    var result  = await queryUtil.queryAllData()
+    var resultData  = await JSON.parse(result)
+    
+    console.log(resultData)
+    // res.send(resultData)
+    res.render('index', {data:resultData})
 })
+
+router.get('/searchdata', async (req, res , next)=>{
+    console.log(req.query.search)
+    var searchdata = req.query.search
+    var result  = await queryUtil.queryData(searchdata)
+    var resultData  = await JSON.parse(result)
+    
+    console.log(resultData)
+    // res.send(resultData)
+    res.render('searchdata', {data:resultData})
+})
+
 
 module.exports = router;
 
