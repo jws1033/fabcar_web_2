bff8045148d2d49d594e024c1309db6dbc866850
내 이름 <you@example.com>
2020-07-08 11:38:17 +0900

    fabcar

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser')
+var ejs = require('ejs')
+var app = express()
+require('dotenv').config()
+
+var apiRouter = require('./routes/Router')
+
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+
+var cors = require('cors')
+app.use(cors())
+
+app.use('/', apiRouter)
+
+
+var port = process.env.PORT || 3000
+app.listen(port , ()=>{
+    console.log(`Server is Starting at http://localhost:${port}`)
+})
---
+++ routes/Router.js
@@ -0,0 +1,8 @@
+var router = require('express').Router()
+
+router.get('/', (req, res, next)=>{
+    res.render('index')
+})
+
+module.exports = router;
+
